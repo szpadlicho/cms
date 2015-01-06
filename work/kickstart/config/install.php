0bd86e197db0fa32c4d5898780e77945d88e145c
DOM <szpadlicho@gmail.com>
2015-01-06 23:49:44 +0100

    start

--- work/kickstart/config/install.php
+++ work/kickstart/config/install.php
@@ -75,19 +75,21 @@ include 'prefix.php';
 $install = new Install();
 $arr_row = array(
     'id'            => 'integer auto_increment',
-    'first_name'    =>'VARCHAR(50) NOT NULL',
-    'last_name'     =>'VARCHAR(50) NOT NULL',
+    'first_name'    =>'VARCHAR(50)',
+    'last_name'     =>'VARCHAR(50)',
     'email'         =>'VARCHAR(50) NOT NULL UNIQUE',
-    'password'      =>'VARCHAR(50) NOT NULL',
-    'phone'         =>'VARCHAR(50) NOT NULL',
-    'country'       =>'VARCHAR(50) NOT NULL',
-    'post_code'     =>'VARCHAR(50) NOT NULL',
-    'town'          =>'VARCHAR(50) NOT NULL',    
-    'street'        =>'VARCHAR(50) NOT NULL',
-    'active'        =>'VARCHAR(50) NOT NULL',
-    'pref'          =>'VARCHAR(50) NOT NULL',
+    'password'      =>'VARCHAR(50)',
+    'phone'         =>'VARCHAR(50)',
+    'country'       =>'VARCHAR(50)',
+    'post_code'     =>'VARCHAR(50)',
+    'town'          =>'VARCHAR(50)',    
+    'street'        =>'VARCHAR(50)',
+    'active'        =>'VARCHAR(50)',
+    'pref'          =>'VARCHAR(50)',
     'create_data'   =>'DATETIME NOT NULL',
-    'update_data'   =>'DATETIME NOT NULL'
+    'update_data'   =>'DATETIME NOT NULL',
+    'facebook_id'   =>'INT',
+    'facebook_link' =>'VARCHAR(100)'
     );
 $arr_val = null;
 $install->installTB($pref.'users', $arr_row, $arr_val);
