22b93473022bdfa25f83adb78d1c7c311d9d0781
Piotr Szpanelewski <szpadlicho@op.pl>
2024-11-07 14:23:14 +0100

    Powrót

---
+++ config.php
@@ -0,0 +1,21 @@
+	<?php
+	// private $host='localhost';
+	// private $port='';
+	// private $dbname='szpadlic_cms';
+	// private $charset='utf8';
+	// private $user='user';
+	// private $pass='user';
+	?>
+	
+<?php
+define('DB_HOST', 'localhost');
+define('DB_PORT', '');
+define('DB_NAME', 'szpadlic_cms');
+define('DB_CHARSET', 'utf8');
+define('DB_USER', 'user');
+define('DB_PASSWORD', 'user');
+define('DB_ENCODING', 'utf8');
+define('DB_SCHEMA', 'information_schema');
+define('ORDER', 'DESC');
+define('DEFAULT_ON', 20);
+define('INTERVAL', '1 MONTH');
